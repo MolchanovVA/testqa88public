f972925e39758560f927bcf04acfd5468311b636
MolchanovVA <108559406+MolchanovVA@users.noreply.github.com>
2022-08-18 06:03:52 +0300

    Homework Mod 12.7

---
+++ mod_12_7.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = input("Сумма вклада")
+valuesList = list(per_cent.values())
+result = [num * float(money) * 0.01 for num in valuesList]
+print(result)
+max_profit = max(result)
+print("Максимальная сумма, которую вы можете заработать -", max_profit)
