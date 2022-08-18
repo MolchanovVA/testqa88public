707f375064c1a83271ad4e14b2488b174104e3e3
MolchanovVA <108559406+MolchanovVA@users.noreply.github.com>
2022-08-18 06:09:53 +0300

    Update mod_12_7.py

--- mod_12_7.py
+++ mod_12_7.py
@@ -2,6 +2,7 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = input("Сумма вклада")
 valuesList = list(per_cent.values())
 result = [num * float(money) * 0.01 for num in valuesList]
-print(result)
+print("Money =", money)
+print("Deposit =",result)
 max_profit = max(result)
 print("Максимальная сумма, которую вы можете заработать -", max_profit)
